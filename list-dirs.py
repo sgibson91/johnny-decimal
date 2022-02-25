fc7c4731b65d032343f0dc36572e2bb59b957915
Sarah Gibson <drsarahlgibson@gmail.com>
2022-02-25 22:27:45 +0000

    Add Python script to list lowest directories in a path

---
+++ list-dirs.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import sys
+
+import pandas as pd
+
+
+def parse_args(args: list):
+    parser = argparse.ArgumentParser(
+        description="List the lowest subdirectories in a specified directory and generate a CSV file."
+    )
+
+    parser.add_argument(
+        "target_path",
+        type=str,
+        help="Path to the target directory",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="subdirs.csv",
+        help="Output CSV file of subdirectory filepaths",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+
+    target_path = os.path.abspath(os.path.expanduser(args.target_path))
+    df = pd.DataFrame(columns=["filepath", "dirname", "area", "category", "delete"])
+
+    for dir_name, subdirs, files in os.walk(target_path):
+        if not subdirs:
+            tmp = pd.DataFrame(
+                {
+                    "filepath": os.path.dirname(dir_name),
+                    "dirname": os.path.basename(dir_name),
+                    "area": "",
+                    "category": "",
+                    "delete": False,
+                },
+                index=[0],
+            )
+            df = pd.concat([df, tmp], ignore_index=True)
+
+    df.to_csv(args.output, index=False)
+
+
+if __name__ == "__main__":
+    main()
